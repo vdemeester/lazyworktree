95b02d4dec908dc3c7017841fe36f934b5c6dc7b
Chmouel Boudjnah <chmouel@chmouel.com>
2026-01-01 07:22:28 +0100

    feat: Add script for releasing new versions
    Added a bash script to automate the release process. This script allows users
    to specify a version to release or interactively bump the major, minor, or
    patch version of the current tag. It ensures the user is on the `main` branch,
    tags the new release, and pushes the tag and main branch to origin.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

---
+++ hack/make-release.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -euf
+VERSION=${1-""}
+
+bumpversion() {
+  current=$(git describe --tags $(git rev-list --tags --max-count=1) || true)
+  if [[ -z ${current} ]]; then
+    current=0.0.0
+  fi
+  echo "Current version is ${current}"
+
+  major=$(uv run --with semver python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_major()))" ${current})
+  minor=$(uv run --with semver python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_minor()))" ${current})
+  patch=$(uv run --with semver python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_patch()))" ${current})
+
+  echo "If we bump we get, Major: ${major} Minor: ${minor} Patch: ${patch}"
+  read -p "To which version you would like to bump [M]ajor, Mi[n]or, [P]atch or Manua[l]: " ANSWER
+  if [[ ${ANSWER,,} == "m" ]]; then
+    mode="major"
+  elif [[ ${ANSWER,,} == "n" ]]; then
+    mode="minor"
+  elif [[ ${ANSWER,,} == "p" ]]; then
+    mode="patch"
+  elif [[ ${ANSWER,,} == "l" ]]; then
+    read -p "Enter version: " -e VERSION
+    return
+  else
+    print "no or bad reply??"
+    exit
+  fi
+  VERSION=$(uv run --with semver python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_${mode}()))" ${current})
+  [[ -z ${VERSION} ]] && {
+    echo "could not bump version automatically"
+    exit
+  }
+  echo "Releasing ${VERSION}"
+}
+
+[[ $(git rev-parse --abbrev-ref HEAD) != main ]] && {
+  echo "you need to be on the main branch"
+  exit 1
+}
+[[ -z ${VERSION} ]] && bumpversion
+
+git tag -s ${VERSION} -m "Releasing version ${VERSION}"
+git push --tags origin ${VERSION}
+git push origin main
